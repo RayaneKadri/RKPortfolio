0fe9c0f6c608da9a0d59ad5db54ad80fc6a443bd
Rayane KADRI <kadrirayane@gmail.com>
2022-01-05 11:05:09 +0100

    création du contenu de la vue "footer"

---
+++ views/V_footer.php
@@ -0,0 +1,62 @@
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4">
+                        <h2>Étudiant en Alternance </h2>
+                            <p>J'ai choisi de suivre le BTS SIO en apprentissage 
+                                car je souhaitais pouvoir apprendre directement en entreprise "sur le terrain"
+                                en parallèle de mes études au lycée ce qui me permet de devenir plus responsable
+                                en me confrontant directement aux contraintes et aux objectifs à atteindre.
+                            </p>
+                            <p><a class="btn btn-primary" href="https://fr.m.wikipedia.org/wiki/Formation_par_alternance" target="_blank"
+                                role="button">En savoir plus »</a>
+                            </p>
+                    </div>
+                        <div class="col-lg-4">
+                            <h2>Le BTS SIO</h2>
+                            <p>Passionné d'informatique depuis toujours, j'aime découvrir et
+                                apprendre tout ce qui touche au domaine du développement 
+                                Informatique. Le BTS SIO s'est présenté comme une évidence surtout
+                                l'apprentissage qui me permet de me former directement en immersion
+                                dans le coeur du métier.
+                                Je sais que grâce à cette formation je vais pouvoir devenir
+                                Développeur Informatique.
+                            </p>
+                            <p><a class="btn btn-primary" href="https://fr.wikipedia.org/wiki/Brevet_de_technicien_supérieur_-_Services_informatiques_aux_organisations"
+                                target="_blank" role="button">En savoir plus »</a>
+                            </p>
+                        </div>
+                        <div class="col-lg-4">
+                            <h2>Option B SLAM</h2>
+                            <p>
+                                L'option B SLAM ("Solutions Logicielles et Applications Métiers") est
+                                l'option par défaut quand on choisit la voie de l'apprentissage.
+                                Travailler en équipe à la satisfaction de besoins d'un client, définir
+                                des spécifications, faire des tests, corriger des bugs, faire de la maintenance,
+                                veiller à la sécurité des données à caractère personnel...
+                                tout en apprenant les bases du métier de développeur :
+                                modélisation des données avec merise et UML, programmation objet avec Java, 
+                                C# et PHP, conception de bases de données sous Postgresql et My Sql,
+                                réalisation de composants logiciels avec Symfony, gestion de projet, méthodes agiles...
+                                C'est totalement ce que je cherchais quand j'ai décidé de venir en SIO.
+                            </p>
+                            <p><a class="btn btn-primary" href="https://www.onisep.fr/Ressources/Univers-Formation/Formations/Post-bac/bts-services-informatiques-aux-organisations-option-b-solutions-logicielles-et-applications-metiers" target="_blank"
+                                role="button">En savoir plus »</a>
+                            </p>
+                        </div>
+                </div>
+                    <hr>
+                    <p class="text-center" id="copyright">© Rayane KADRI 2021</p>
+            </div>
+        </footer>
+                <!-- Optional JavaScript; choose one of the two! -->
+
+                <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+                <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+                        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+                crossorigin="anonymous"></script>
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"
+                        integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns"
+                crossorigin="anonymous"></script>
+    </body>
+</html>
