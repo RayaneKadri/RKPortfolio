84388e02905eacfb63a5b88c2095e383540ca35f
Rayane KADRI <kadrirayane@gmail.com>
2022-01-05 12:51:17 +0100

    Ajout du chemin vers la favicon dans le controller

--- pages/C_cv.php
+++ pages/C_cv.php
@@ -3,6 +3,7 @@ require_once(__DIR__ . '/../views/V_head.php');
 ?>
 <link rel="stylesheet" href="/../css/styles.css">
 <link rel="stylesheet" href="/../css/cv.css">
+<link rel="icon" href="../favicon.ico">
 <?php $page=1;
     require_once(__DIR__ . '/../views/V_navbar.php');
     require_once(__DIR__ . '/../views/V_cv.php');
