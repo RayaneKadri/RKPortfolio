ed4f2a53f1ffd623cd44d04a7c291d2c06aaa3f0
Rayane KADRI <kadrirayane@gmail.com>
2022-01-05 12:49:00 +0100

    ajout des chemins vers les feuilles de styles css

--- pages/C_cv.php
+++ pages/C_cv.php
@@ -1,6 +1,8 @@
 <?php
 require_once(__DIR__ . '/../views/V_head.php');
 ?>
+<link rel="stylesheet" href="/../css/styles.css">
+<link rel="stylesheet" href="/../css/cv.css">
 <?php $page=1;
     require_once(__DIR__ . '/../views/V_navbar.php');
     require_once(__DIR__ . '/../views/V_cv.php');
