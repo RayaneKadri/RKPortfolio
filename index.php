392183584c20a7b3bd22442c35eda68de0149004
Rayane KADRI <kadrirayane@gmail.com>
2022-01-05 12:13:10 +0100

    correction V_portfolio --> V_home

--- index.php
+++ index.php
@@ -3,6 +3,6 @@ require_once(__DIR__ . '/views/V_head.php');
 ?>
 <?php $page=0;
     require_once(__DIR__ . '/views/V_navbar.php');
-    require_once(__DIR__ . '/views/V_portfolio.php');
+    require_once(__DIR__ . '/views/V_home.php');
     require_once(__DIR__ . '/views/V_footer.php');
 ?>
