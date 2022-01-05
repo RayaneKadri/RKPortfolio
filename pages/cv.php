090fff79b416b7d44d426d12f1e7d3ab71775f26
Rayane KADRI <kadrirayane@gmail.com>
2022-01-05 10:35:12 +0100

    retour en html

--- pages/cv.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once(__DIR__ . '/../php/header.php');
-?>
-        <link rel="stylesheet" href="/../css/cv.css">
-        <?php $page=1;
-        require_once(__DIR__ . '/../php/navbar.php');
-        require_once(__DIR__ . '/../php/V_cv.php');
-        require_once(__DIR__ . '/../php/footer.php'); ?>
