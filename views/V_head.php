35171e38a1a1f4b7f7cc7bbba74a909e64afa08d
Rayane KADRI <kadrirayane@gmail.com>
2022-01-05 12:45:35 +0100

    chemin vers styles.css retiré de la vue

--- views/V_head.php
+++ views/V_head.php
@@ -13,8 +13,6 @@
               href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
               integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"
               crossorigin="anonymous">
-         <!-- fichier Styles CSS -->
-         <link rel="stylesheet" href="./css/styles.css">
         <link rel="icon" href="favicon.ico">
         <title>Accueil</title>
     </head>
