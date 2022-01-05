3e9a839948fe45e33ff465a3ac5f6ff2e3ed8fdf
Rayane KADRI <kadrirayane@gmail.com>
2022-01-05 11:05:51 +0100

    création de la vue de la page d'accueil

---
+++ views/V_home.php
@@ -0,0 +1,8 @@
+    <div class="jumbotron">
+                <h1>Bienvenue</h1>
+                <img src="./images/moi.PNG" alt="Ma Photo">
+                <p>Je m'appelle Rayane KADRI, j'ai 19 ans et ce site a pour vocation de me présenter en tant que personne,
+                    en tant qu'étudiant apprenti et candidat à l'obtention du BTS SIO (Services Informatiques aux Organisations).        
+                </p>
+                <a class="btn btn-primary" href="./pages/cv.html"_blank" role="button">En savoir plus »</a>
+    </div> <!-- Emplacement pour le contenu principal de la page -->
