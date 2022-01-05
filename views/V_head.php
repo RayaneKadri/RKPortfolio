195aeec0485764635a2ff32138eaac6d378c34c1
Rayane KADRI <kadrirayane@gmail.com>
2022-01-05 11:05:34 +0100

    création du contenu de la vue de l'en tête

---
+++ views/V_head.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="Portfolio de Rayane KADRI - BTS SIO SLAM">
+        <meta name="author" content="Rayane KADRI">
+        <!-- Bootstrap CSS -->
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+              rel="stylesheet">
+        <link rel="stylesheet"
+              href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+              integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"
+              crossorigin="anonymous">
+         <!-- fichier Styles CSS -->
+         <link rel="stylesheet" href="./css/styles.css">
+        <link rel="icon" href="favicon.ico">
+        <title>Mon Portfolio</title>
+    </head>
