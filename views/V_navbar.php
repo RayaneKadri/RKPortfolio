fb7aee403d7e44e879639f3ff670a9dec2328cd3
Rayane KADRI <kadrirayane@gmail.com>
2022-01-05 11:06:02 +0100

    création de la vue pour la navbar

---
+++ views/V_navbar.php
@@ -0,0 +1,42 @@
+<body>
+    <nav class="navbar navbar-dark bg-dark fixed-top navbar-expand-md">
+                <div class="container">
+                    <button type="button" class="navbar-toggler" data-toggle="collapse"
+                            data-target="#navbar" aria-controls="navbar">
+                        <span class="sr-only">Navigation raccourcie</span>☰
+                    </button>
+                    <a class="navbar-brand" href="index.html">Rayane KADRI</a>
+                    <div id="navbar" class="navbar-collapse collapse">
+                        <ul class="nav navbar-nav">
+                            <li class="active nav-item">
+                                <a href="index.html" class="nav-link">
+                                    <span class="fa fa-home"></span> Accueil
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="pages/cv.html" class="nav-link">
+                                    <span class="fa fa-id-badge"></span> CV
+                                </a>
+                            <li class="dropdown nav-item">
+                                <a href="#" class="dropdown-toggle nav-link"
+                                data-toggle="dropdown" role="button">
+                                    <span class="fa fa-list-alt"></span> Réalisations <span class="caret"></span>
+                                </a>
+                                <ul class="dropdown-menu" role="menu">
+                                    <li role="presentation" class="dropdown-header dropdown-item">
+                                        Au Lycée</li>
+                                    <li role="presentation" class="dropdown-item">
+                                        <a role="menuitem" href="pages/nolark.html">Nolark</a>
+                                    </li>
+                                    <li role="presentation" class="dropdown-divider"></li>
+                                    <li role="presentation" class="dropdown-header dropdown-item">
+                                        En entreprise</li>
+                                    <li role="presentation" class="dropdown-item">
+                                        <a role="menuitem" href="pages/firstyear.html">Projet 1ére année</a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+            </div>
+    </nav>
