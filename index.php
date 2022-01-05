11ca05314e396b446c560f3ecb0323a9603cbe28
Rayane KADRI <kadrirayane@gmail.com>
2022-01-05 12:52:13 +0100

    ajout du chemin vers la feuille de style dans le controller

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 require_once(__DIR__ . '/views/V_head.php');
 ?>
+<link rel="stylesheet" href="/../css/styles.css">
 <?php $page=0;
     require_once(__DIR__ . '/views/V_navbar.php');
     require_once(__DIR__ . '/views/V_home.php');
