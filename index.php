ac447106ae2a636c6332a13c03509e73121d1a7d
Rayane KADRI <kadrirayane@gmail.com>
2022-01-05 12:10:43 +0100

    index.html --> index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+require_once(__DIR__ . '/views/V_head.php');
+?>
+<?php $page=0;
+    require_once(__DIR__ . '/views/V_navbar.php');
+    require_once(__DIR__ . '/views/V_portfolio.php');
+    require_once(__DIR__ . '/views/V_footer.php');
+?>
