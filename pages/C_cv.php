60b9ba57d1477dca4a11d89e11e8e0716e92a5f6
Rayane KADRI <kadrirayane@gmail.com>
2022-01-05 12:42:09 +0100

    correction chemin vers les vues

--- pages/C_cv.php
+++ pages/C_cv.php
@@ -1,8 +1,8 @@
 <?php
-require_once(__DIR__ . '../views/V_head.php');
+require_once(__DIR__ . '/../views/V_head.php');
 ?>
 <?php $page=1;
-    require_once(__DIR__ . '../views/V_navbar.php');
-    require_once(__DIR__ . '../views/V_cv.php');
-    require_once(__DIR__ . '../views/V_footer.php');
+    require_once(__DIR__ . '/../views/V_navbar.php');
+    require_once(__DIR__ . '/../views/V_cv.php');
+    require_once(__DIR__ . '/../views/V_footer.php');
 ?>
