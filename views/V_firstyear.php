48eb2c90c054e4be5fbc322534f9357b533f71e2
Rayane KADRI <kadrirayane@gmail.com>
2022-01-05 13:32:50 +0100

    mise en place du contenu de la vue de firstyear

---
+++ views/V_firstyear.php
@@ -0,0 +1,10 @@
+<section class="jumbotron text-center">
+            <div class="container">
+                <h1 class="jumbotron-heading">Première projet de 1ère année</h1>
+                <img src="../images/warning.PNG" alt="photo représentant un panneau Danger">
+                <p class="lead text-muted">jusqu'à prèsent , je n'ai pas eu encore la possibilité d'aborder de projet,
+                    je suis actuellement en auto formation sur les langages que je vais utiliser au sein de mon entreprise (DIRISI CDAD TOULON).
+                    Pour avoir plus de précision sur les langages, je vous invite à aller sur la page <a href="./cv.html">CV</a>.
+                </p>
+            </div>
+</section>
