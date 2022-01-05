ce4e4dec224722164d48b29e047dac0404f580ca
Rayane KADRI <kadrirayane@gmail.com>
2022-01-05 12:50:07 +0100

    chemin vers la favicon retirée

--- views/V_head.php
+++ views/V_head.php
@@ -13,6 +13,5 @@
               href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
               integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"
               crossorigin="anonymous">
-        <link rel="icon" href="favicon.ico">
         <title>Accueil</title>
     </head>
