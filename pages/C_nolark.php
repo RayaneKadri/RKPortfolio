95bf3730adca1b14a3f9b86a1b39a83656d9bac1
Rayane KADRI <kadrirayane@gmail.com>
2022-01-05 13:31:08 +0100

    ajout du chemin vers le fichier de style realisations.css

--- pages/C_nolark.php
+++ pages/C_nolark.php
@@ -2,6 +2,7 @@
 require_once(__DIR__ . '/../views/V_head.php');
 ?>
 <link rel="stylesheet" href="/../css/styles.css">
+<link rel="stylesheet" href="/../css/realisations.css">
 <link rel="icon" href="../favicon.ico">
 <?php $page=2;
     require_once(__DIR__ . '/../views/V_navbar.php');
