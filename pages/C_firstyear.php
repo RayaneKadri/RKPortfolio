b2263cb1577da0453f76c3210142fbd1e76a60de
Rayane KADRI <kadrirayane@gmail.com>
2022-01-13 16:16:17 +0100

    contenu retiré suite  à variabilisation

--- pages/C_firstyear.php
+++ pages/C_firstyear.php
@@ -1,10 +1,3 @@
-<?php
-$title='Projet de première année';
-require_once(__DIR__ . '/../views/templates/head.php');
-?>
-<link rel="stylesheet" href="/../css/styles.css">
-<link rel="stylesheet" href="/../css/realisations.css">
-<link rel="icon" href="../favicon.ico">
 <?php $page=2;
     require_once(__DIR__ . '/../views/templates/navbar.php');
     require_once(__DIR__ . '/../views/V_firstyear.php');
