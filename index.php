8e50a01a705cf4dedc2625c536b192aa7620348b
Rayane KADRI <kadrirayane@gmail.com>
2022-01-13 16:43:46 +0100

    suppression require_once vers navbar.php

--- index.php
+++ index.php
@@ -1,10 +1,4 @@
 <?php
-$title='Accueil';
-require_once(__DIR__ . '/views/templates/head.php');
-?>
-<link rel="stylesheet" href="/../css/styles.css">
-<?php $page=0;
-    require_once(__DIR__ . '/views/templates/navbar.php');
     require_once(__DIR__ . '/views/V_home.php');
     require_once(__DIR__ . '/views/templates/footer.php');
 ?>
--- pages/C_cv.php
+++ pages/C_cv.php
@@ -1,5 +1,4 @@
-<?php $page=1;
-    require_once(__DIR__ . '/../views/templates/navbar.php');
+<?php
     require_once(__DIR__ . '/../views/V_cv.php');
     require_once(__DIR__ . '/../views/templates/footer.php');
 ?>
