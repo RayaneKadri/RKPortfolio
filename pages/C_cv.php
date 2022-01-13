1cd2315c65cb3c9be7311c1762ddacebc3ace4b4
Rayane KADRI <kadrirayane@gmail.com>
2022-01-13 16:09:09 +0100

    modification du controlleur suite a la variabilisation de la page

--- pages/C_cv.php
+++ pages/C_cv.php
@@ -1,10 +1,3 @@
-<?php
-$title='CV';
-require_once(__DIR__ . '/../views/templates/head.php');
-?>
-<link rel="stylesheet" href="/../css/styles.css">
-<link rel="stylesheet" href="/../css/cv.css">
-<link rel="icon" href="../favicon.ico">
 <?php $page=1;
     require_once(__DIR__ . '/../views/templates/navbar.php');
     require_once(__DIR__ . '/../views/V_cv.php');
