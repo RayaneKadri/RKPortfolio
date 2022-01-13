58aea257cbc1212199ea9a29451288fd45779f79
Rayane KADRI <kadrirayane@gmail.com>
2022-01-13 13:11:22 +0100

    modifications des chemins d'accés vers head, navbar et footer

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 $title='Accueil';
-require_once(__DIR__ . '/views/V_head.php');
+require_once(__DIR__ . '/views/templates/head.php');
 ?>
 <link rel="stylesheet" href="/../css/styles.css">
 <?php $page=0;
-    require_once(__DIR__ . '/views/V_navbar.php');
+    require_once(__DIR__ . '/views/templates/navbar.php');
     require_once(__DIR__ . '/views/V_home.php');
-    require_once(__DIR__ . '/views/V_footer.php');
+    require_once(__DIR__ . '/views/templates/footer.php');
 ?>
