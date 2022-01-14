f08fe14fc6a11df2e83254fbcd3bff0b66416183
Rayane KADRI <kadrirayane@gmail.com>
2022-01-14 09:22:21 +0100

    passage des controllers en fonction

---
+++ pages/controller.php
@@ -0,0 +1,22 @@
+<?php
+
+function home() {
+    require_once(__DIR__ . '/../views/V_home.php');
+    require_once(__DIR__ . '/../views/templates/footer.php');
+}
+
+
+function cv() {
+    require_once(__DIR__ . '/../views/V_cv.php');
+    require_once(__DIR__ . '/../views/templates/footer.php');
+}
+
+function nolark() {
+    require_once(__DIR__ . '/../views/V_nolark.php');
+    require_once(__DIR__ . '/../views/templates/footer.php');
+}
+
+function firstyear() {
+    require_once(__DIR__ . '/../views/V_firstyear.php');
+    require_once(__DIR__ . '/../views/templates/footer.php');
+}
