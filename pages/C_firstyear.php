c5fe42d53f8af27b1d331a13406cb19a158e5665
Rayane KADRI <kadrirayane@gmail.com>
2022-01-14 08:15:02 +0100

    $page dans la vue de firstyear

--- pages/C_firstyear.php
+++ pages/C_firstyear.php
@@ -1,5 +1,4 @@
-<?php $page=2;
-    require_once(__DIR__ . '/../views/templates/navbar.php');
+<?php
     require_once(__DIR__ . '/../views/V_firstyear.php');
     require_once(__DIR__ . '/../views/templates/footer.php');
 ?>
--- views/V_firstyear.php
+++ views/V_firstyear.php
@@ -13,3 +13,4 @@
 </section>
 <?php $content= ob_get_clean(); ?>
 <?php require(__DIR__ . '/../views/templates/head.php'); ?>
+<?php require(__DIR__ . '/../views/templates/navbar.php'); ?>
